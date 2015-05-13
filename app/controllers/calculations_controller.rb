934bc496b8187aa03ed88ee9de8feff2bc1e049a
Larsam123 <larsen.samuel@gmail.com>
2015-05-13 15:59:18 -0500

    practice_complete

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -2,9 +2,21 @@ class CalculationsController < ApplicationController
   def instructions
   end
 
+  def sqr
+    @num = params[:number].to_f
+    @square = @num**2
+  end
+
   def sqrt
+    @num = params[:number].to_f
+    @square_root = (@num**(0.5)).round(2)
   end
 
   def pmt
+    @apr = params[:apr].to_f
+    @years = params[:mths].to_i/12
+    @principal = params[:principal].to_f
+
+    @monthly_payment =(((@apr/1200)*@principal)/(1-(1+@apr/1200)**(-1*@years*12))).round(2)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   get("/", { :controller => "calculations", :action => "instructions" })
+
+  get("/square/:number", {:controller => "calculations", :action => "sqr"})
+  get("/square_root/:number", {:controller => "calculations", :action => "sqrt"})
+
+  get("/payment/:apr/:mths/:principal", {:controller => "calculations", :action => "pmt"})
+
 end
